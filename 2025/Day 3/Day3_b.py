ba3f4291ede820495dc3551b6220ebe51d7ecf04
antho <anthony.sims.j@gmail.com>
2025-12-06 10:06:19 -0500

    remove unneeded import from timing

--- 2025/Day 3/Day3_b.py
+++ 2025/Day 3/Day3_b.py
@@ -1,14 +1,13 @@
 #! python3 
 
-import sys,time
+import sys
 joltage = 0 
 for bank in sys.stdin:
     bl = [int(i) for i in bank.strip()]
     sbl = bl[:]
-    mi = 0 
     for i in range (12,0,-1):
         max_index = len(sbl)+1
-        di = sbl.index(max(sbl[mi:max_index-i]))
+        di = sbl.index(max(sbl[0:max_index-i]))
         joltage += (sbl[di] * (10**(i-1)))
         sbl = sbl[di+1:]      
 print(joltage)
